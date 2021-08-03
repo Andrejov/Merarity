7a98010b6d9923b337a10a013b352fd3688d737b
_Andrejov <ziomek0012@gmail.com>
2021-08-03 21:10:57 +0200

    +express

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const Merarity = require('./dist/index');
+const express = require('express');
 
 const logger = new Merarity.Logger();
 
+const app = express();
 const cfg = new Merarity.MerarityConfig('./config.json', logger);
 const bot = new Merarity.Merarity(cfg, logger);
+
+app.get('', (req,res) => {
+    res.writeHead(200);
+    res.write('Merarity is working');
+});
+
+// eslint-disable-next-line no-undef
+app.listen(process.env.PORT || 5000);
 
 bot.run();
